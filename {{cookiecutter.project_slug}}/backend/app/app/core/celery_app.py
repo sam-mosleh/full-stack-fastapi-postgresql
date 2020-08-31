47c39c876542dc32fb2377dbd1fcb221afef301b
Sam Mosleh <sam.mosleh@ut.ac.ir>
2020-08-31 18:11:45 +0430

    Added Redis backend to Celery.

--- {{cookiecutter.project_slug}}/backend/app/app/core/celery_app.py
+++ {{cookiecutter.project_slug}}/backend/app/app/core/celery_app.py
@@ -2,6 +2,6 @@ from celery import Celery
 
 from app.core.config import settings
 
-celery_app = Celery("worker", broker=settings.REDIS_DSN)
+celery_app = Celery("worker", broker=settings.REDIS_DSN, backend=settings.REDIS_DSN)
 
 celery_app.conf.task_routes = {"app.worker.test_celery": "main-queue"}
