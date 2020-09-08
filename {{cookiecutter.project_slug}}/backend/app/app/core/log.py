9fe24709cad09f02aa18c412631f049c1f0e30ca
Sam Mosleh <sam.mosleh@ut.ac.ir>
2020-09-08 17:29:16 +0430

    Added unified logger. Updated some of dependencies.

--- {{cookiecutter.project_slug}}/backend/app/app/core/log.py
+++ {{cookiecutter.project_slug}}/backend/app/app/core/log.py
@@ -1,5 +1,45 @@
 import logging
 
+from loguru import logger as loguru_logger
+
 from app.core.config import settings
 
-logger = logging.getLogger(settings.LOGGER)
+
+class InterceptHandler(logging.Handler):
+    loglevel_mapping = {
+        logging.CRITICAL: "CRITICAL",
+        logging.ERROR: "ERROR",
+        logging.WARNING: "WARNING",
+        logging.INFO: "INFO",
+        logging.DEBUG: "DEBUG",
+        logging.NOTSET: "NOTSET",
+    }
+
+    def emit(self, record):
+        try:
+            level = loguru_logger.level(record.levelname).name
+        except AttributeError:
+            level = self.loglevel_mapping[record.levelno]
+
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        log = loguru_logger.bind(request_id="app")
+        log.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
+
+intercept_handler = InterceptHandler()
+logging.root.setLevel(settings.LOG_LEVEL.upper())
+for name in (
+    "gunicorn",
+    "gunicorn.access",
+    "gunicorn.error",
+    "uvicorn",
+    "uvicorn.access",
+    "uvicorn.error",
+):
+    logging.getLogger(name).handlers = [intercept_handler]
+
+logger = loguru_logger.bind(request_id=None, method=None)
--- {{cookiecutter.project_slug}}/backend/app/app/core/socket.py
+++ {{cookiecutter.project_slug}}/backend/app/app/core/socket.py
@@ -1,5 +1,8 @@
 import socketio
 
 from app.core.config import settings
+from app.core.log import logger
 
-external_sio = socketio.AsyncRedisManager(settings.REDIS_DSN, write_only=True)
+external_sio = socketio.AsyncRedisManager(
+    settings.REDIS_DSN, write_only=True, logger=logger
+)
