d733d719f5f241a33bd54d2204a0e066368fa11d
Sam Mosleh <sam.mosleh@ut.ac.ir>
2020-10-15 05:47:37 +0330

    Improved tests for items.

--- {{cookiecutter.project_slug}}/backend/app/app/tests/crud/test_item.py
+++ {{cookiecutter.project_slug}}/backend/app/app/tests/crud/test_item.py
@@ -1,49 +1,40 @@
 from sqlalchemy.orm import Session
 
 from app import crud
+from app.models.item import Item
+from app.models.user import User
 from app.schemas.item import ItemCreate, ItemUpdate
 from app.tests.utils.user import create_random_user
 from app.tests.utils.utils import random_lower_string
 
 
-def test_create_item(db: Session) -> None:
+def test_create_item(db: Session, new_user: User) -> None:
     title = random_lower_string()
     description = random_lower_string()
     item_in = ItemCreate(title=title, description=description)
-    user = create_random_user(db)
-    item = crud.item.create_with_owner(db=db, obj_in=item_in, owner_id=user.id)
+    item = crud.item.create_with_owner(db=db, obj_in=item_in, owner_id=new_user.id)
     assert item.title == title
     assert item.description == description
-    assert item.owner_id == user.id
+    assert item.owner_id == new_user.id
 
 
-def test_get_item(db: Session) -> None:
-    title = random_lower_string()
-    description = random_lower_string()
-    item_in = ItemCreate(title=title, description=description)
-    user = create_random_user(db)
-    item = crud.item.create_with_owner(db=db, obj_in=item_in, owner_id=user.id)
-    stored_item = crud.item.get(db=db, id=item.id)
+def test_get_item(db: Session, new_item: Item) -> None:
+    stored_item = crud.item.get(db=db, id=new_item.id)
     assert stored_item
-    assert item.id == stored_item.id
-    assert item.title == stored_item.title
-    assert item.description == stored_item.description
-    assert item.owner_id == stored_item.owner_id
+    assert new_item.id == stored_item.id
+    assert new_item.title == stored_item.title
+    assert new_item.description == stored_item.description
+    assert new_item.owner_id == stored_item.owner_id
 
 
-def test_update_item(db: Session) -> None:
-    title = random_lower_string()
+def test_update_item(db: Session, new_item: Item) -> None:
     description = random_lower_string()
-    item_in = ItemCreate(title=title, description=description)
-    user = create_random_user(db)
-    item = crud.item.create_with_owner(db=db, obj_in=item_in, owner_id=user.id)
-    description2 = random_lower_string()
-    item_update = ItemUpdate(description=description2)
-    item2 = crud.item.update(db=db, db_obj=item, obj_in=item_update)
-    assert item.id == item2.id
-    assert item.title == item2.title
-    assert item2.description == description2
-    assert item.owner_id == item2.owner_id
+    item_update = ItemUpdate(description=description)
+    updated_item = crud.item.update(db=db, db_obj=new_item, obj_in=item_update)
+    assert new_item.id == updated_item.id
+    assert new_item.title == updated_item.title
+    assert updated_item.description == description
+    assert new_item.owner_id == updated_item.owner_id
 
 
 def test_delete_item(db: Session) -> None:
